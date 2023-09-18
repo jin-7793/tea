e22e0d7ec19e0e8c5d0aae1dc73b79782ec03013
Jin-7793 <soccer4484@gmail.com>
2023-09-18 21:14:31 +0900

    view表示の練習

---
+++ resources/views/posts/test.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body>
+        <h1>こんにちは</h1>
+
+    </body>
+</html>
