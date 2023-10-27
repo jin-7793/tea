b42d35e0cf84cade6bfdb8f410bdbb7c9eebc018
Jin-7793 <soccer4484@gmail.com>
2023-10-27 22:31:35 +0900

    投稿とアカウントを削除処理の修正

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
+use App\Models\User;
 
 class ProfileController extends Controller
 {
@@ -51,6 +52,8 @@ class ProfileController extends Controller
         Auth::logout();
         
 
+        $user->userlikes()->delete();
+        $user->posts()->delete();
         $user->delete();
 
         $request->session()->invalidate();
