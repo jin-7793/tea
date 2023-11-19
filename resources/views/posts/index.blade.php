32499d80f96d123502f5c472f730dd2b7710c2e0
Jin-7793 <soccer4484@gmail.com>
2023-11-19 21:39:01 +0900

    編集機能実装など

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -20,42 +20,48 @@
             <div class="post mt-4 p-8 bg-white w-full rounded-2xl">
                 <div class="title text-lg font-semibold">
                     <font size="5">
-                        <a href="posts/{{$post->id}}">{{$post->title}}</a>
+                         <a href="{{$post->id}}">{{$post->title}}</a>
                     </font>
                     <a>投稿者:{{$post->user->name}}</a>
                 </div>
                 <hr class="w-full">
                 <div class="body mt-2">
-                    {{$post->body}}
-                    <div class="text-xs text-gray-400 text-right">
-                        有効期限:{{$post->expired_at}}
+                    {!! nl2br(e($post->body)) !!}
+                    <br><br>
+                    <div class="flex">
+                        <div class="flex-1">
+                            @if($post->like()->where('user_id','=',Auth::user()->id)->exists())
+                                {{--もうすでにいいねしている投稿だったらいいねを赤く表示する--}}
+                                <a href='/unlike/{{$post->id}}' class="ml-2 mt-2 border-2 bg-red-500">いいね</a>
+                            @else
+                                <a href='/like/{{$post->id}}'>いいね</a>
+                            @endif
+                            <a>{{$post->like->count()}}</a>
+                        </div>
+                        <div class="text-xs text-gray-400 text-right flex-2">
+                            有効期限:{{$post->expired_at}}
+                        </div>
                     </div>
-                    @if($post->like()->where('user_id','=',Auth::user()->id)->exists())
-                        {{--もうすでにいいねしている投稿だったらいいねを赤く表示する--}}
-                        <a href='/unlike/{{$post->id}}' class="ml-2 mt-2 border-2 bg-red-500">いいね</a>
-                    @else
-                    <a href='/like/{{$post->id}}'>いいね</a>
+                     @if($post->user()->where('id','=',Auth::user()->id)->exists())
+                        <form action="/posts/{{$post->id}}" id="form_{{$post->id}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" onclick="deletePost({{$post->id}})">
+                                delete
+                            </button>
+                        </form>
                     @endif
-                    <a>{{$post->like->count()}}</a>
-                    
-                    <form action="/posts/{{$post->id}}" id="form_{{$post->id}}" method="post">
-                        @csrf
-                        @method('DELETE')
-                        <button type="button" onclick="deletePost({{$post->id}})">
-                            delete
-                        </button>
-                    </form>
                 </div>
             </div>
         @endforeach
     </div>
     <script>
-    function deletePost(id){
-        if (confirm('削除すると復元できません。\n　本当に削除しますか？')){
-            document.getElementById(`form_${id}`).submit();
-            
+        function deletePost(id)
+        {
+            if (confirm('削除すると復元できません。\n　本当に削除しますか？'))
+            {
+                document.getElementById(`form_${id}`).submit();
+            }
         }
-        
-    }
     </script>
 </x-app-layout>
--- resources/views/reception.blade.php
+++ resources/views/reception.blade.php
@@ -4,7 +4,7 @@
     <!--Fonts-->
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 </head>
-<x-app-layout>
+<x-guest-layout>
     <x-slot name="header">
         <h1 class="font-semibold text-xl text-gray-800 leading-tight">
             Feel Free!!
@@ -15,18 +15,27 @@
             Feel Free!! を始めよう!
         </div>
         <div class="flex">
-            <a href="{{ route('register') }}" class="flex-1">
-                <x-primary-button class="bg-red-500 ml-8 mt-8">
-                    Register
-                </x-primary-button>
-            </a>
-            <a href="{{ route('login') }}" class="flex-2">
-                <x-primary-button class="bg-red-500 ml-8 mt-8">
-                    Login
-                </x-primary-button>
-            </a>
+             @if (Route::has('login'))
+                @auth
+                    <a href="{{ route('index') }}">
+                        <x-primary-button class="bg-red-500 ml-8 mt-8">
+                            Let's Go!!
+                        </x-primary-button>
+                    </a>
+                @else
+                    <a href="{{ route('register') }}" class="flex-1">
+                        <x-primary-button class="bg-red-500 ml-8 mt-8">
+                            Register
+                        </x-primary-button>
+                    </a>
+                    <a href="{{ route('login') }}" class="flex-2">
+                        <x-primary-button class="bg-red-500 ml-8 mt-8">
+                            Login
+                        </x-primary-button>
+                    </a>
+                @endauth
+            @endif
         </div>
     </div>
-    
-</x-app-layout>
+</x-guest-layout>
     
